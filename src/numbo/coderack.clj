5939ffd13b87cc9945e388e82c1d08f01d7c86bf
Tom Hume <twhume@gmail.com>
2020-04-13 21:04:38 -0700

    Renamed "node" to "codelet" in the coderack

--- src/numbo/coderack.clj
+++ src/numbo/coderack.clj
@@ -1,15 +1,14 @@
-(ns numbo.coderack
-	(:require [numbo.codelet :refer :all]))
+(ns numbo.coderack)
 
 (def CODERACK (atom '()))
 
 ; What are urgency value ranges?
-; Imagine we have a set of nodes in the rack with urgencies 1 1 2 5 10
+; Imagine we have a set of codelet in the rack with urgencies 1 1 2 5 10
 ; The urgency value ranges are 1 2 4 9 19
 ; (minimum urgency must be 1 rather than 0, so that 0-urgency nodes have a chance of running)
 ; A random value from 0..19 would be generated to choose one weighted by its urgency
 
-(defn -select-node-for-val
+(defn -select-codelet-for-val
  "Given a sequence of urgency value ranges and nodes, and a value v within the ranges, return a node"
 	[urgencies v]
  (second (first (filter #(< v (first %)) urgencies))))
@@ -19,16 +18,21 @@
  [rack]
  	 (map #(list %1 %2) (reductions + (map :urgency rack)) rack))
 
-(defn -select-next-node
+(defn -select-next-codelet
  "Grabs the next codelet from the rack r, probabilistically according to its urgency"
  ([rack]
  	(let [urgencies (-make-urgencies rack)]
  	 (if (not-empty urgencies)
- 	  (-select-node-for-val urgencies (rand-int (first (last urgencies))))))))
+ 	  (-select-codelet-for-val urgencies (rand-int (first (last urgencies))))))))
 
-(defn process-next-node
+(defn process-next-codelet
  "Grabs and executes a codelet from the rack, removing it afterwards"
  ([]
- 	(let [node (-select-next-node @CODERACK)]
+ 	(let [codelet (-select-next-codelet @CODERACK)]
  	 ; TODO operate the node
- 	 (reset! CODERACK (let [[n m] (split-with (partial not= node) @CODERACK)] (concat n (rest m)))))))
+ 	 (reset! CODERACK (let [[n m] (split-with (partial not= codelet) @CODERACK)] (concat n (rest m)))))))
+
+(defn add-codelet
+ "Adds a new codelet to the coderack"
+ ([r c] (conj r c))
+ ([c] (reset! CODERACK (add-codelet @CODERACK c))))
