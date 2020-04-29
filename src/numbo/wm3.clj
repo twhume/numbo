258f099495a33b62d1697518b38dbfad43beeb3e
Tom Hume <twhume@gmail.com>
2020-04-28 21:22:17 -0700

    Changed param1,param2 to be a vector of parameters

--- src/numbo/wm3.clj
+++ src/numbo/wm3.clj
@@ -17,7 +17,7 @@
 
 ; BRICKS and TARGETS are lists of Entries.
 ; Entries are maps with a :value, a random :uuid and an :attr(activeness)
-; Entries in BLOCKS can also have an :op(erator) and 2 children, each themselves block entries
+; Entries in BLOCKS can also have an :op(erator) and a seq of 2 children, each themselves block entries
 ; (so Entries in BLOCKS are each trees)
 
 (defn -initial-attr
@@ -34,7 +34,7 @@
 (defn -new-entry
  "Creates a new memory entry structure"
  ([v] (hash-map :value v :uuid (misc/uuid) :attr (-initial-attr v)))
- ([v o p1 p2] (assoc (-new-entry v) :op o :param1 p1 :param2 p2)))
+ ([v o p] (assoc (-new-entry v) :op o :params p)))
 
 (defn -find-block
  "Finds a block in blocks-list bl using its UUID u"
@@ -61,15 +61,22 @@
 
 (defn add-block
  "Adds a new block to memory"
-	([bl value op param1 param2] (conj bl (-new-entry value op param1 param2)))
-	([value op param1 param2] (reset! BLOCKS (add-block @BLOCKS value op param1 param2))))
+	([bl value op p] (conj bl (-new-entry value op p)))
+	([value op p] (reset! BLOCKS (add-block @BLOCKS value op p))))
 
 (defn add-child-block
  "Adds a child to a block in memory bl, by its uuid"
- [blocks par-uuid par-param value op param1 param2]
- (let [parent (-find-block blocks par-uuid)
- 						new-node (assoc parent par-param (-new-entry value op param1 param2))]
- 						(-replace-block blocks parent new-node)))
+ ([blocks par-uuid par-param value op p]
+		(let [parent (-find-block blocks par-uuid)
+								new-node (assoc parent :params (assoc (:params parent) par-param (-new-entry value op p)))]
+								(-replace-block blocks parent new-node)))
+ ([par-uuid par-param value op p]
+ 	(reset! BLOCKS (add-child-block @BLOCKS par-uuid par-param value op p))))
+
+
+;TODO: extend add-child-block to look inside sub-nodes of all blocks, as well as at the root level
+; (means recursing in -find-block?)
+;TODO: write graph converter for resulting structures, using PROPER Hofstadter notation!
 
 
 (set-target 100)
@@ -78,11 +85,8 @@
 (add-brick 10)
 (add-brick 7)
 
-(add-block 50 :times 5 10)
-(add-block 70 :times 7 10)
-(add-block 35 :times 5 7)
+(add-block 50 :times [5 10])
+(add-block 70 :times [7 10])
+(add-block 35 :times [5 7])
 
-
-
-(def blocks '(1 (2 (4) (5)) (3 (6) (7))))
 
