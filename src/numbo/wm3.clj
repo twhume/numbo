0f1a3e3e92e3db44f77892be1c444647afdeb62b
Tom Hume <twhume@gmail.com>
2020-05-01 21:49:38 -0700

    Got graph being created and plotted

--- src/numbo/wm3.clj
+++ src/numbo/wm3.clj
@@ -13,7 +13,7 @@
 (def TARGET (atom nil))
 (def BLOCKS (atom '()))
 
-; BRICKS and TARGETS are lists of Entries.
+; BRICKS is a list of Entries, TARGET is an Entry
 ; Entries are maps with a :value, a random :uuid and an :attr(activeness)
 ;
 ; Entries in BLOCKS can also have an :op(erator) and a vector of 2 children, each ints or block entries
@@ -26,21 +26,10 @@
  [v]
  1)
 
-(defn -replace-block
- "Replace block a in list bl with block b"
- [bl a b]
- (conj (remove #{a} bl) b))
-
 (defn -new-entry
  "Creates a new memory entry structure"
  ([v] (hash-map :value v :uuid (misc/uuid) :attr (-initial-attr v)))
  ([v o p] (assoc (-new-entry v) :op o :params p)))
-
-(defn -find-block
- "Finds a block in blocks-list bl using its UUID u"
- [bl u]
- (let [root-matches (filter #(= u (:uuid %1)) bl)]
- 	(if (nil? root-matches) nil (first root-matches))))
 
 (defn -make-blocktree-zipper
  "Make a clojure zipper from the blocktree bt"
@@ -105,7 +94,129 @@
  ([par-uuid par-param value op p]
  	(reset! BLOCKS (add-child-block @BLOCKS par-uuid par-param value op p))))
 
-;TODO: write graph converter for resulting structures, using PROPER Hofstadter notation!
+;----- all this should go into viz.clj eventually -----
+
+(defn -is-virt-uuid?
+ "Is the supplied string a virtual UUID?"
+	[s]
+	(cond
+		(.endsWith s "_param0") true
+		(.endsWith s "_param1") true
+		(.endsWith s "_op") true
+		:else false
+))
+
+(defn -get-virt-uuid
+ "Returns the UUID part of a virtual node UUID"
+ [u]
+ (clojure.string/replace u #"_.*$" ""))
+
+(defn -get-virt-param
+ "Returns the param part of a virtual node UUID"
+ [u]
+ (clojure.string/replace u #"^.*_" ""))
+
+(defn -node-to-graph
+	"Converts the node at a zipper loc to its Rhizome representation"
+	[loc]
+	(let [node (zip/node loc)]
+		(list (:uuid node) (vector (str (:uuid node) "_op") (:params node))))
+)
+
+; Either the UUIDs of sub-blocks or "virtual UUIDs" referring to params
+
+(defn -block-children
+	"Returns vector of UUID representations of node n"
+	[n]
+	(let [children (:params n)
+							uuid (:uuid n)]
+		(vec (map-indexed #(if (int? %2) (str uuid "_param" %1) (:uuid %2)) children))))
+
+(defn -blocktree-to-graph
+	"Convert a single blocktree into a Rhizome-format graph"
+	[bt]
+	(let [zipper (-make-blocktree-zipper bt)]
+		(loop [cur zipper out '{}]
+		 (if (zip/end? cur) out
+		 	(let [node (zip/node cur)
+		 							node-uuid (:uuid node)
+		 							node-op-uuid (str node-uuid "_op")
+		 							node-children (-block-children node)
+		 							]
+			  	(recur
+			  		(zip/next cur)
+			  		(do
+			  			(if (int? node) out
+					  	(assoc out
+					  		node-uuid (vector node-op-uuid)
+					  		node-op-uuid node-children
+					  		(first node-children) '[]
+					  		(second node-children) '[]
+			  		))
+			 	)))))))
+
+(defn -to-graph
+ "Convert a target, bricks and blocks into a graph for Rhizome"
+ [ta br bl]
+ (let [bt-graphs (map -blocktree-to-graph bl)]
+ 	(apply merge bt-graphs)))
+
+; add root nodes for all the MAGIC child IDs
+
+(defn -attractiveness-to-color
+	"Handles coloring nodes by attractiveness"
+	[a]
+	(let [r (int ( * 255 (float (/ (- 3 a) 3))))
+							g (- 255 r)
+							b (- 255 r)]
+		(format "#FF%02X%02X" r r)))
+
+(defn -get-wm-style
+ "Works out appropriate style for a type t"
+ [t]
+ (case t
+ 	:target "bold"
+ 	:secondary "bold"
+ 	:brick "solid"
+ 	:block "dashed"
+ ))
+
+
+(def -op-names '{ :times "X" :plus "+" :minus "-"})
+
+(defn -get-node-label
+ "Given the UUID u of a block in the list of blocks bl, return its label"
+	[bl u]
+
+	(let [node-uuid (if (-is-virt-uuid? u) (-get-virt-uuid u) u)
+							entry (zip/node (first (filter (complement nil?) (map #(-find-blocktree-loc (-make-blocktree-zipper %) node-uuid) bl))))]
+
+							(if (-is-virt-uuid? u)
+								(condp = (-get-virt-param u)
+									"op" [((:op entry) -op-names) :op]
+									"param0" [(first (:params entry)) :param]
+									"param1" [(second (:params entry)) :param]
+									"ERROR")
+							[(:value entry) :result])))
+
+(defn view-graph
+ "Show the graph for the working memory target, bricks and blocks"
+ ([ta br bl]
+ (let [g (-to-graph ta br bl)]
+	 (rh/view-graph (keys g) g
+	 	:directed? false
+ 	 :options {:concentrate true :layout "neato" :mode "hier" :model "circuit"}
+ 		:node->descriptor (fn [u]
+
+ 		  (let [[label type] (-get-node-label bl u)]
+ 		  		(condp = type
+ 		  		 :op (hash-map :label label :style "rounded,filled" :fontcolor "white" :fontsize 12 :fixedsize "true" :labelloc "c" :width 0.4 :height 0.4 :color "black" )
+ 		  		 :param (hash-map :label label :style "solid" :fontcolor "black" :fontsize 12 :fixedsize "false" :labelloc "c" :width 0.4 :height 0.4 :color "red" )
+ 		  		 :result (hash-map :label label :style "rounded,filled" :fontcolor "white" :fontsize 12 :fixedsize "false" :labelloc "c" :width 0.4 :height 0.4 :color "black" )
+ 		  		 :else (println "WEIRD TYPE" type)))))))
+ ([] (view-graph @TARGET @BRICKS @BLOCKS)))
+
+
 ;TODO: write unit tests
 
 (set-target 100)
@@ -115,7 +226,8 @@
 (add-brick 7)
 
 (add-block 50 :times [5 10])
-(add-block 70 :times [7 10])
-(add-block 35 :times [5 7])
+(add-block 70 :plus [7 10])
+(def u (:uuid (first (add-block 35 :times [5 7]))))
+(add-child-block u 1 99 :minus [3 33])
 
 
