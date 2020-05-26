ce9009a0ad476def65742a840225b06c77710e61
Tom Hume <twhume@gmail.com>
2020-05-25 21:15:37 -0700

    Started using tools.logging and log4j for debugging purposes

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
+  															[org.clojure/clojure "1.10.0"]
+  															[org.clojure/tools.logging "1.1.0"]
   															[rhizome "0.2.9"]
   															[seesaw/seesaw "1.5.0"]]
   :main ^:skip-aot numbo.core
