60a4f584c299fd9d88c44696746e9cd3da0013aa
Tom Hume <twhume@gmail.com>
2020-07-30 21:04:45 -0700

    First two runs and an automated harness for them

--- README.md
+++ README.md
@@ -16,6 +16,13 @@ lein run -- -t 114 -b 1,6,7,11,20 -i 1000 -c 10
 lein test - unit tests
 lein repl - loads all code, run (-main) to trigger the GUI and leave a REPL open
 
+Dump CSV output at the end of the Raw data tab of
+https://docs.google.com/spreadsheets/d/1WZ5VMy2MDzJz80Jh93ApQpbrfS-tuu16n_Xqfur-Egk/edit#gid=0
+
+## Tags
+
+v1.01 first fully running version with automated tests
+
 ## License
 
 Copyright © 2020 Tom Hume
@@ -30,6 +37,12 @@ Public License, v. 2.0 are satisfied: GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or (at your
 option) any later version, with the GNU Classpath Exception which is available
 at https://www.gnu.org/software/classpath/license.html.
+
+
+* I notice that when I change the target from 114 to 97, no solution is found - but (7 * 11) + 20 would work
+* - actually, a solution is found at iteration 17036(!) - but there's an issue here for sure.
+* Next step: make 10 problems, benchmark them 100 times and work from here
+
 
 * Make the current iteration an input field which you can put a value into
 
--- resources/paper-puzzles.sh
+++ resources/paper-puzzles.sh
@@ -2,26 +2,27 @@
 
 # Script to run the set of canonical problems from the Numbo paper
 
-ITERATIONS=10000
-COUNT=500
+ITERATIONS=5000
+COUNT=100
 
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 TAG=`git tag`
 ARGS="-i $ITERATIONS -c $COUNT"
-OUT="run-$TAG-$current_time.csv"
+OUT="runs/run-$TAG-$current_time.csv"
 CMD="lein run --"
+PRE="s/^/$current_time,$TAG,/"
 
 # main example of the paper
-$CMD -t 114 -b 11,20,7,1,6 -i $ARGS >> $OUT
-$CMD -t 87 -b 8,3,9,10,7 -i $ARGS >> $OUT
-$CMD -t 31 -b 3,5,24,3,14 -i $ARGS >> $OUT
-$CMD -t 25 -b 8,5,5,11,2 -i $ARGS >> $OUT
-$CMD -t 81 -b 9,7,2,25,18 -i $ARGS >> $OUT
-$CMD -t 6 -b 3,3,17,11,22 -i $ARGS >> $OUT
-$CMD -t 11 -b 2,5,1,25,23 -i $ARGS >> $OUT
-$CMD -t 116 -b 20,2,16,14,6 -i $ARGS >> $OUT
-$CMD -t 127 -b 7,6,4,22,25 -i $ARGS >> $OUT
-$CMD -t 41 -b 5,16,22,25,1 -i $ARGS >> $OUT
+$CMD -t 114 -b 11,20,7,1,6 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 87 -b 8,3,9,10,7 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 31 -b 3,5,24,3,14 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 25 -b 8,5,5,11,2 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 81 -b 9,7,2,25,18 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 6 -b 3,3,17,11,22 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 11 -b 2,5,1,25,23 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 116 -b 20,2,16,14,6 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 127 -b 7,6,4,22,25 -i $ARGS | sed $PRE >> $OUT
+$CMD -t 41 -b 5,16,22,25,1 -i $ARGS | sed $PRE >> $OUT
 
 
